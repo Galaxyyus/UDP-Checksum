8a0293de580734cf8163de78f58a44c2c1a93c86
CleverFox29 <romilaroy30@gmail.com>
2026-06-23 10:29:18 +0530

    natural typing sim

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@
 #include <chrono>
 #include <ctime>
 
-std::optional<std::uint16_t> generate_checksum(std::istream& in) {
+std::optional<std::uint16_t> generate_checksum(std::istream &in)
+{
 	const size_t CHUNK_SIZE = 64 * 1024 * 1024;
 	std::vector<std::uint8_t> buffer(CHUNK_SIZE);
 
@@ -20,28 +21,34 @@ std::optional<std::uint16_t> generate_checksum(std::istream& in) {
 	std::uint8_t leftover_byte = 0;
 	size_t leftover_count = 0;
 
-	while (true) {
-		if (leftover_count > 0) buffer[0] = leftover_byte;
+	while (true)
+	{
+		if (leftover_count > 0)
+			buffer[0] = leftover_byte;
 
-		in.read(reinterpret_cast<char*>(buffer.data() + leftover_count), CHUNK_SIZE - leftover_count);
+		in.read(reinterpret_cast<char *>(buffer.data() + leftover_count), CHUNK_SIZE - leftover_count);
 
 		// Error checks
-		if (in.bad()) {
+		if (in.bad())
+		{
 			std::cerr << "Error: input stream encountered an I/O error!\n";
 			return std::nullopt;
 		}
-		if (in.fail() && !in.eof()) {
+		if (in.fail() && !in.eof())
+		{
 			std::cerr << "Error: input stream encounter an unknown error!\n";
 			return std::nullopt;
 		}
 
 		auto bytes_read = in.gcount();
-		if (bytes_read == 0) break; // Reached EOF
+		if (bytes_read == 0)
+			break; // Reached EOF
 		auto total_bytes = bytes_read + leftover_count;
 		leftover_count = 0;
 
 		size_t i = 0;
-		while (i + 1 < total_bytes) {
+		while (i + 1 < total_bytes)
+		{
 			std::uint8_t a = buffer[i++];
 			std::uint8_t b = buffer[i++];
 
@@ -50,18 +57,21 @@ std::optional<std::uint16_t> generate_checksum(std::istream& in) {
 			checksum += octet_pair;
 		}
 
-		if (i < total_bytes) {
+		if (i < total_bytes)
+		{
 			leftover_byte = buffer[i];
 			leftover_count = 1;
 		}
 	}
 
-	if (leftover_count > 0) {
+	if (leftover_count > 0)
+	{
 		std::uint16_t octet_pair = leftover_byte << 8;
 		checksum += octet_pair;
 	}
 
-	while (checksum >> 16) {
+	while (checksum >> 16)
+	{
 		checksum = (checksum & 0xFFFF) + (checksum >> 16);
 	}
 
@@ -71,27 +81,40 @@ std::optional<std::uint16_t> generate_checksum(std::istream& in) {
 		return ~checksum;
 }
 
+int main(const int argc, const char *argv[])
+{
 
-int main(const int argc, const char* argv[]) {
-
-	//checking if an argument is already there
-	if (argc < 2){
+	// checking if an argument is already there
+	if (argc < 2)
+	{
 		// TODO: please replace the placeholder in the instructions file :)
 		std::ifstream file("instructions.txt");
 		char c;
 		srand(time(0));
-		
+
+		int speed = 70; // millis per char
+
+		int chars = 1 + rand() % 10;
+		int wait = (1 + rand() % speed);
+
 		while (file.get(c))
 		{
-			std::cout<< c;
-			std::this_thread::sleep_for(std::chrono::milliseconds(1+ rand()%50));
+			if (chars <= 0)
+			{
+				chars = 1 + rand() % 10;
+				wait = (1 + rand() % speed);
+			}
+			std::cout << c;
+			std::cout.flush();
+			std::this_thread::sleep_for(std::chrono::milliseconds(wait));
+			chars--;
 		}
 		return 1;
 	}
 
-
 	const std::string option = argv[1];
-	if (option != "-g" && option != "--generate" && option != "-c" && option != "--check") {
+	if (option != "-g" && option != "--generate" && option != "-c" && option != "--check")
+	{
 		std::cout << "Unknown option: " << option << " !\n";
 		return 1;
 	}
@@ -100,67 +123,84 @@ int main(const int argc, const char* argv[]) {
 
 	std::string inline_input, input_filename;
 
-	for (int i = 2; i < argc; i++) {
+	for (int i = 2; i < argc; i++)
+	{
 		const std::string arg = argv[i];
 
 		// Inline input
-		if (arg[0] != '-') {
-			if (inline_input.empty()) {
+		if (arg[0] != '-')
+		{
+			if (inline_input.empty())
+			{
 				inline_input = arg;
 				continue;
-			} else {
+			}
+			else
+			{
 				std::cout << "Error: Only one inline input can be provided!\n";
 				return 1;
 			}
 		}
 		// Filename argument
-		else if (arg == "-f" || arg == "--file") {
-			if (i + 1 == argc) {
+		else if (arg == "-f" || arg == "--file")
+		{
+			if (i + 1 == argc)
+			{
 				std::cout << "Error: No filename provided after -f or --file flag!\n";
 				return 1;
-			} else {
+			}
+			else
+			{
 				input_filename = argv[++i];
 			}
 		}
 		// Invalid flags
-		else {
+		else
+		{
 			std::cout << "Unknown flag: " << arg << " !\n";
 			return 1;
 		}
 	}
 
 	// Both or neither input file and inline input provided
-	if (!inline_input.empty() && !input_filename.empty() && is_generate) {
+	if (!inline_input.empty() && !input_filename.empty() && is_generate)
+	{
 		std::cout << "Error: cannot use both -f and an inline input string with -g!\n";
 		return 1;
 	}
-	if (inline_input.empty() && input_filename.empty()) {
+	if (inline_input.empty() && input_filename.empty())
+	{
 		std::cout << "Error: no input provided. Use -f <path> or pass a string directly!\n";
 		return 1;
 	}
 
 	// Handle checksum generation
 	std::optional<std::uint16_t> checksum;
-	if (!input_filename.empty()) {
+	if (!input_filename.empty())
+	{
 		std::ifstream input(input_filename, std::ios::binary);
-		if (!input) {
+		if (!input)
+		{
 			std::cerr << "Error: could not open file: " << input_filename << '\n';
 			return 1;
 		}
 
 		checksum = generate_checksum(input);
 	}
-	if (!inline_input.empty()) {
+	if (!inline_input.empty())
+	{
 		std::istringstream input(inline_input);
 		checksum = generate_checksum(input);
 	}
 
-	if (!checksum) {
+	if (!checksum)
+	{
 		std::cout << "Aborting, operation failed!!!\n";
 		return 1;
 	}
 
-	if (is_generate) {
+	if (is_generate)
+	{
 		std::cout << std::bitset<16>(*checksum) << '\n';
 		return 0;
 	}
